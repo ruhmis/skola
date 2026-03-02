6ead0ef4caa4859ae659d542b8cde28e99a6ead8
ruhmis <156013029+ruhmis@users.noreply.github.com>
2026-03-02 08:42:04 +0000

    Rolands

---
+++ dots_teikums.py
@@ -0,0 +1,20 @@
+a=input()
+
+if a[-1] == '.':
+    print("stāstījuma")
+elif a[-1] == '!':
+    print("rosinājuma")
+elif a[-1] == '?':
+    print("jautājuma")
+
+print(a.count(" ") + 1) # vārdu skaits teikumā
+
+for i in range(0, len(a)-1):
+    if a[i] == " ":
+        print(a[i-1], end=" ")
+print(a[-2], end=" ")
+
+#
+#
+#
+    
