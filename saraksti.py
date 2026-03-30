e267d64da9c8b3c95169930a3b413672fc526a62
ruhmis <156013029+ruhmis@users.noreply.github.com>
2026-03-30 08:18:19 +0000

    commit

---
+++ saraksti.py
@@ -0,0 +1,12 @@
+    # 1. No teikuma vārdiem izveidot sarakstu.
+    # 2. Ar type() funkcijas palīdzību izdrukāt saraksta datu tipu.
+    # 3. Aiz saraksta elementa ar indeksu 2 ievietot vārdu “Sveiks”.
+    # 4. Sakārtot sarakstu pretēji alfabētiskajai secībai.
+    # 5. Izdrukāt kurā vietā tagad atrodas vārds Sveiks.
+    # 6. Vai sarakstā ir vārds “abc”? Izdrukāt JA vai NE.
+    # 7. Vai teikuma pēdējais vārds atrodas trešajā vietā? Izdrukāt JA vai NE.
+
+teikums = input()
+vardi = teikums.split()
+
+insert(2, "Sveiks")
---
+++ sarksti02.py
@@ -0,0 +1,6 @@
+    # 1. No teikuma vārdiem (ar visām pieturzīmēm pie tiem) izveidot sarakstu.
+    # 2. Vai sarakstā ir vārds, kurš satur tieši 3 burtus a? Izdrukāt JA vai NE.
+    # 3. Izdrukāt visas pieturzīmes pretējā secībā, nekā tās dotas teikumā (izdrukāt vienā rindā).
+    # 4. No saraksta izdzēst visus vārdus “abc”.
+
+    # 5. Visiem saraksta vārdiem aizvākt visas pieturzīmes.
