4bb8c81ac6a7e4aaf1926df926f7ea475719e368
ruhmis <156013029+ruhmis@users.noreply.github.com>
2026-04-21 06:40:02 +0000

    commit

--- projekta darbs/tictactoe.py
+++ projekta darbs/tictactoe.py
@@ -2,20 +2,20 @@
 # https://docs.google.com/document/d/1Cz-u84URpiy417TUwpI00pXLKeB02WcDePsRlz6uXLI/edit?usp=sharing
 
 # izveido laukumu
-board = [" " for i in range(9)]
+laukums = [" " for i in range(9)]
 
 # funkcija, kas izdrukā laukumu
-def print_board():
+def print_laukums():
     print()
     for i in range(3):
         for j in range(3):
             index = i * 3 + j
             
             # ja tukšs, rāda numuru
-            if board[index] == " ":
+            if laukums[index] == " ":
                 print(index + 1, end="")
             else:
-                print(board[index], end="")
+                print(laukums[index], end="")
             
             if j < 2:
                 print(" | ", end="")
@@ -34,35 +34,35 @@ def check_winner(player):
     ]
     
     for pos in win_positions:
-        if board[pos[0]] == player and board[pos[1]] == player and board[pos[2]] == player:
+        if laukums[pos[0]] == player and laukums[pos[1]] == player and laukums[pos[2]] == player:
             return True
     return False
 
 # spēle
-current_player = "X"
+pasreiz_spele = "X"
 
 for turn in range(9):
-    print_board()
+    print_laukums()
     
-    move = int(input("Spēlētājs " + current_player + ", izvēlies (1-9): "))
+    move = int(input("Spēlētājs " + pasreiz_spele + ", izvēlies (1-9): "))
     
-    if board[move - 1] == " ":
-        board[move - 1] = current_player
+    if laukums[move - 1] == " ":
+        laukums[move - 1] = pasreiz_spele
     else:
         print("Šī vieta jau aizņemta!")
         continue
     
-    if check_winner(current_player):
-        print_board()
-        print("Spēlētājs", current_player, "uzvarēja!")
+    if check_winner(pasreiz_spele):
+        print_laukums()
+        print("Spēlētājs", pasreiz_spele, "uzvarēja!")
         break
     
     # maina spēlētāju
-    if current_player == "X":
-        current_player = "O"
+    if pasreiz_spele == "X":
+        pasreiz_spele = "O"
     else:
-        current_player = "X"
+        pasreiz_spele = "X"
 
 else:
-    print_board()
+    print_laukums()
     print("Neizšķirts!")
